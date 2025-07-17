8c3b0f5bd16444077bb951a91ec89724707e84d2
hugodesenv <hugosouzadev@hotmail.com>
2025-07-17 13:34:24 -0300

    Cors

--- api.aspnetcore.webfinancas/Infrastructure/Extension/CorsExtension.cs
+++ api.aspnetcore.webfinancas/Infrastructure/Extension/CorsExtension.cs
@@ -8,7 +8,7 @@
             {
                 option.AddDefaultPolicy(policy =>
                 {
-                    policy.WithOrigins(["http://localhost:3000", "http://127.0.0.1:3000"])
+                    policy.WithOrigins(["http://localhost:3000", "http://127.0.0.1:3000", "http://localhost:3001", "http://127.0.0.1:3001"])
                         .AllowAnyHeader()
                         .AllowAnyMethod()
                         .AllowCredentials();
